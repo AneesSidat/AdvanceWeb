abf8d71a62dd783c19a7543065dbd4542d5b1b1c
Anees Sidat <u1951623@unimail.hud.ac.uk>
2021-10-14 16:48:34 +0100

    Add a visitor model with migrations and seeder. Add to controller and view

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,16 +3,15 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Database\Seeders\VisitorSeeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call([
+            VisitorSeeder::class
+        ]);
     }
 }
+
