33f5cdb1359639dd9f9e2c1dc52e5301d4c5dc10
Anees Sidat (U1951623) <U1951623@unimail.hud.ac.uk>
2021-10-18 14:40:58 +0100

    Add User seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,13 +4,15 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Database\Seeders\VisitorSeeder;
+use Database\Seeders\UserSeeder;
 
 class DatabaseSeeder extends Seeder
 {
     public function run()
     {
         $this->call([
-            VisitorSeeder::class
+            VisitorSeeder::class,
+            UserSeeder::class
         ]);
     }
 }
