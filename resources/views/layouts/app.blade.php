a53b464fc1ef16c999ddb2a7a52debe6dd30a605
Anees Sidat (U1951623) <U1951623@unimail.hud.ac.uk>
2021-10-18 14:55:16 +0100

    Add login view/controller/routes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,11 +17,16 @@
 
         <section class="navigation">
 		<div class="container mx-auto">
-            <ul>
-                <li><a href="/">Home</a></li>
-                <li><a href="/visitors">Visitors</a></li>
-                <li><a href="/visitors/create">Add visitor</a></li>
+            <ul class="navbar">
+                <li><a href="/">Home</a></li>                
+                @if (Auth::guest())
+                    <li><a href="/login">Login</a></li>
+                @else 
+                    <li><a href="/visitors">Visitors</a></li>
+                    <li><a href="/visitors/create">Sign the guestbook</a></li>
+                @endif
             </ul>
+
 		</div>
         </section>
 
